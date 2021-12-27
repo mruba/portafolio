133341727a8ca7e33de97ed65649473aac506ea2
miguel rubalcava <mruba@miguels-Air.hsd1.ca.comcast.net>
2021-12-27 15:45:26 -0800

    about page

--- pages/about/about.tsx
+++ pages/about/about.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import type { NextPage } from "next";
+import Image from 'next/image'
+import profilePic from '../../public/images/profile.png'
 
-interface IProps {}
+interface IProps { }
 
 const About: NextPage<IProps> = function (props) {
   return (
     <div>
+      <Image
+        src={profilePic}
+        width={150}
+        height={150}
+        className="rounded-full"
+        alt="Picture of the author" />
       <p className="text-pink">this is about page</p>
     </div>
   );
