94edb376d9aeb6531e55d4e8013844b8bc629091
miguel rubalcava <mruba@miguels-Air.hsd1.ca.comcast.net>
2021-12-29 19:48:06 -0800

    fixed styles on time line item

--- components/time-line/time-line.tsx
+++ components/time-line/time-line.tsx
@@ -11,25 +11,43 @@ const timeLine = [
         title: "First",
         description: "First description",
         tags: ["first", "first-tag", "first-tag-1", "first-tag-2"],
+        workingPeriod: "2015 - 2017",
     },
     {
         id: "2",
         title: "Second",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
         tags: ["first", "first-tag", "first-tag-1", "first-tag-2"],
+        workingPeriod: "2015 - 2017",
     },
     {
         id: "3",
         title: "Third",
         description: "Third description",
         tags: ["first", "first-tag", "first-tag-1", "first-tag-2"],
+        workingPeriod: "2015 - 2017",
     },
     {
         id: "4",
         title: "Fourth",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
         tags: ["first", "first-tag", "first-tag-1", "first-tag-2"],
-    }
+        workingPeriod: "2015 - 2017",
+    },
+    {
+        id: "5",
+        title: "Second",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        tags: ["first", "first-tag", "first-tag-1", "first-tag-2"],
+        workingPeriod: "2015 - 2017",
+    },
+    {
+        id: "6",
+        title: "Second",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        tags: ["first", "first-tag", "first-tag-1", "first-tag-2"],
+        workingPeriod: "2015 - 2017",
+    },
 ]
 
 
@@ -44,7 +62,7 @@ export default function TimeLine(props: ITimeLineProps) {
         <div className=" py-12 sm:mx-auto px-2 sm:px-6">
 
 
-            <div className="relative text-gray-800 text-sm font-semibold">
+            <div className="relative text-gray-800 text-sm font-semibold space-y-12">
 
                 <div className="hidden sm:block w-3 bg-blue absolute h-full left-1/2 transform -translate-x-1/2"></div>
                 {timeLine.map(timeLineItem)}
