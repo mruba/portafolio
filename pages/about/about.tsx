9cbbe5471d3a69476a622431ac8bae3285f64c06
miguel rubalcava <mruba@miguels-Air.hsd1.ca.comcast.net>
2021-12-29 22:33:56 -0800

    fixed about me styles

--- pages/about/about.tsx
+++ pages/about/about.tsx
@@ -9,15 +9,22 @@ interface IProps { }
 const About: NextPage<IProps> = function (props) {
   return (
     <div>
-      <Image
-        src={profilePic}
-        width={150}
-        height={150}
-        className="rounded-full"
-        alt="Picture of the author"
-      />
-      <p className="text-pink">this is about page</p>
+      <div className="float-left px-6 py-4">
+        <Image
+          src={profilePic}
+          width={150}
+          height={150}
+          className="rounded-full"
+          alt="Picture of the author"
+        />
+      </div>
+      <h1 className="text-white py-3">About Me</h1>
+      <p className="text-pink leading-7 break-all">
+        Lorem ipsum dolor sit amet, vel te sumo definitionem, incorrupte interesset in duo, facete voluptua appetere et mel. At mei apeirian eleifend. Fabulas elaboraret ea sed. Prodesset appellantur delicatissimi qui te, amet primis laboramus mel in. Eos ei eros nemore necessitatibus, amet forensibus instructior est no.
+        Lorem ipsum dolor sit amet, vel te sumo definitionem, incorrupte interesset in duo, facete voluptua appetere et mel. At mei apeirian eleifend. Fabulas elaboraret ea sed. Prodesset appellantur delicatissimi qui te, amet primis laboramus mel in. Eos ei eros nemore necessitatibus, amet forensibus instructior est no.
+      </p>
       <TimeLine />
+      {/* current location button with google maps pin */}
     </div>
   );
 };
