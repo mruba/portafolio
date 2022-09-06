42ad1a8e6890cce41982ef8498b0e230c8b558d1
Miguel Rubalcava <mruba.dev@gmail.com>
2022-09-05 20:34:19 -0700

    fix(layout): change goback button position

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -6,22 +6,29 @@ interface ILayoutProps {
   children: React.ReactNode;
 }
 
+const GoBack = () => {
+  const router = useRouter();
+  return (
+    <div
+      className="p-2 absolute top-0 left-0 text-pink text-xl flex items-center dark:text-white cursor-pointer"
+      onClick={() => router.back()}
+    >
+      <span>◀︎</span>
+      <p className="pl-4">Go back</p>
+    </div>
+  );
+};
+
 const Layout: React.FunctionComponent<ILayoutProps> = function (props) {
   useTheme();
   const router = useRouter();
 
+  const isHomePage = router.pathname !== "/";
+
   return (
     <div className="dark:bg-black h-screen px-5 py-5 overflow-auto">
       {/* TODO: refactor this into a function to make it clean */}
-      {router.pathname !== "/" && (
-        <div
-          className="text-pink text-xl flex items-center dark:text-white cursor-pointer"
-          onClick={() => router.back()}
-        >
-          <span className="h-10">◀︎</span>
-          <p className="pl-4">Go back</p>
-        </div>
-      )}
+      {isHomePage && <GoBack />}
       <div className="container mx-auto">{props.children}</div>
     </div>
   );
